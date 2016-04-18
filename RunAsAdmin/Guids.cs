d6b9ae507a9a3031857837a7603f49ac2d1ddb9e
Samir L. Boulema <sboulema@gmail.com>
2016-04-18 16:04:05 +0200

    Initial commit

---
+++ RunAsAdmin/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace FundaRealEstateBV.RunAsAdmin
+{
+    static class GuidList
+    {
+        public const string guidRunAsAdminPkgString = "f6211522-a083-49ba-aba4-9c082ab8a570";
+        public const string guidRunAsAdminCmdSetString = "69afcb2a-c6c8-4934-bdab-816ed79bd026";
+
+        public static readonly Guid guidRunAsAdminCmdSet = new Guid(guidRunAsAdminCmdSetString);
+    };
+}
---
+++ RunAsAdmin/RunAsAdminPackage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using EnvDTE;
+using System.ComponentModel;
+using System.IO;
+
+namespace FundaRealEstateBV.RunAsAdmin
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [ProvideAutoLoad(UIContextGuids80.NoSolution)]
+    [Guid(GuidList.guidRunAsAdminPkgString)]
+    [ProvideOptionPage(typeof(OptionPageGrid), "RunAsAdmin", "General", 0, 0, true)]
+    public sealed class RunAsAdminPackage : Package
+    {
+        private DTE _dte;
+        private string _devenvFilename;
+        private OptionPageGrid _options;
+        private const string RegistryFolder = @"HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers\";
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            _options = (OptionPageGrid)GetDialogPage(typeof(OptionPageGrid));
+            _dte = (DTE)GetService(typeof(DTE));
+            _devenvFilename = _dte.Application.FileName;
+
+            if (_options.Enabled == true)
+            {
+                var RunAsAdminKeyLauncher = Registry.GetValue(RegistryFolder, @"C:\Program Files (x86)\Common Files\Microsoft Shared\MSEnv\VSLauncher.exe", null);
+                if (RunAsAdminKeyLauncher == null)
+                {
+                    Registry.SetValue(RegistryFolder, @"C:\Program Files (x86)\Common Files\Microsoft Shared\MSEnv\VSLauncher.exe", "RUNASADMIN");
+                }
+
+                var RunAsAdminKeyDevEnv = Registry.GetValue(RegistryFolder, _devenvFilename, null);
+                if (RunAsAdminKeyDevEnv == null || ((string)RunAsAdminKeyDevEnv).Equals("RUNASNORMAL"))
+                {
+                    Registry.SetValue(RegistryFolder, _devenvFilename, "RUNASADMIN");
+                }
+            }
+            else
+            {
+                Registry.SetValue(RegistryFolder, _devenvFilename, "RUNASNORMAL");
+            }
+        }
+    }
+
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [CLSCompliant(false), ComVisible(true)]
+    public class OptionPageGrid : DialogPage
+    {
+        [Category("RunAsAdmin")]
+        [DisplayName(@"Enabled")]
+        [Description("Run Visual Studio as administrator")]
+        public bool Enabled { get; set; }
+    }
+}
